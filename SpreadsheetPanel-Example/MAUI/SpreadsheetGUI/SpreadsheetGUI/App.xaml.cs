25324526a2eecbf67f9e39821f606dbc588b2d2e
Habtamu <habtu.bt@gmail.com>
2025-10-26 13:30:50 +0900

    AS6 Grid panel Example

---
+++ SpreadsheetPanel-Example/MAUI/SpreadsheetGUI/SpreadsheetGUI/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace SpreadsheetGUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = MainPage = new NavigationPage(new MainPage());
+
+        }
+    }
+}
