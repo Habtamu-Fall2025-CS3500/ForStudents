d01a7c63107ddbf060b7f2d06b6ea7b0ca2f428e
Habtamu <habtu.bt@gmail.com>
2025-11-06 13:02:11 +0900

    Lab 8

---
+++ Labs/Lab8-ThreadPractice/ThreadPractice/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab9_ThreadPractice
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            // Launch the demo
+            ThreadPractice t1 = new ThreadPractice();
+            t1.demo();
+
+        }
+    }
+}
